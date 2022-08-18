93f67c8ae849061cf64b86112dae3102e173fe75
Goldie5fnf <82838084+Goldie5fnf@users.noreply.github.com>
2022-08-18 19:46:52 +0300

    Cyka

--- source/Discord.hx
+++ source/Discord.hx
@@ -71,16 +71,7 @@ class DiscordClient
 			endTimestamp = startTimestamp + endTimestamp;
 		}
 
-		//DiscordRpc.presence({
-			details: details,
-			state: state,
-			largeImageKey: 'icon',
-			largeImageText: "Engine Version: " + MainMenuState.psychEngineVersion,
-			smallImageKey : smallImageKey,
-			// Obtained times are in milliseconds so they are divided so Discord can use it
-			startTimestamp : Std.int(startTimestamp / 1000),
-			endTimestamp : Std.int(endTimestamp / 1000)
-		});
+		//DiscordRpc.presence
 
 		//trace('Discord RPC Updated. Arguments: $details, $state, $smallImageKey, $hasStartTimestamp, $endTimestamp');
 	}
